ff5f8d8f69ad2c2d59f2fb44996e3c2cebb92269
SongHwaJun <hwajun.song@danbplus.com>
2024-08-17 20:46:02 +0900

    20240817_v1_튜토리얼 커밋

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import ErrorPage from "./error-page";
+import Root      from "./routes/root";
+import Contact   from "./routes/contact";
+
+// 튜토리얼: https://reactrouter.com/en/main/start/tutorial
+// 현재 브렌치 조회: git branch -a
+// 브랜치 생성: git checkout -b 브랜치명
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    // element: <div>Hello world</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
