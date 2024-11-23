83fe04cf677e16f8da67163bfe3dab691c993149
전준영 <reus7042@gmail.com>
2024-11-23 12:52:23 +0900

    ⚡️ modify api source for preparing error

--- src/pages/join/api/join.js
+++ src/pages/join/api/join.js
@@ -4,14 +4,18 @@ import { POST } from "@shared/api"
 export  const joinRequest = async ({request}) => {
     
     const res = await POST('/member/join', request)
-    debugger
+    
     console.log(res)
 
     if (res.state === 201) {
 
         alert('회원가입에 성공했습니다.')
         window.location = '/'
-    } else 
-        return alert('회원가입이 정상적으로 처리되지 않았습니다.')
+        return true
+        
+    } else  {
+        alert('회원가입이 정상적으로 처리되지 않았습니다.')
+        return false
+    }
 
 }
