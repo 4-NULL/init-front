1005343bb9b477e6172ed4e2f44000e257f8b3de
Lim dae-gyeong <lds7575@naver.com>
2024-12-21 10:58:02 +0900

    로그인/비로그인 분기

--- src/pages/home/ui/HomePage.jsx
+++ src/pages/home/ui/HomePage.jsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 
 import { GroupItem } from '@entities/group';
 
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 
 export function HomePage() {
 
+  const savedUser = localStorage.getItem("user");
+  const isLoggedIn = savedUser !== null; // 로그인 여부 확인
   const groups = useLoaderData();
 
   useEffect(() => {
@@ -16,9 +18,20 @@ export function HomePage() {
   return (
     <div className="p-4">
       <div className='flex flex-col gap-2'>
-        {
-          groups.map(group => <GroupItem key={group.seq} data={group} />)
-        }
+      {isLoggedIn ? (
+        <div>
+          {
+            groups.map(group => <GroupItem key={group.seq} data={group} />)
+          }
+        </div>
+
+      ): (
+        <div>
+        {/* 로그아웃한 사용자에게 메시지 또는 로그인 프롬프트 렌더링 */}
+        <h2>로그인 후 그룹을 확인하세요.</h2>
+        <Link to="/login">로그인 페이지로 이동</Link>
+      </div>
+      )}
       </div>
     </div>
   );
