bc318d8167093e7a7745ef749cc873333825452d
전준영 <reus7042@gmail.com>
2025-01-18 10:31:06 +0900

    :fire: remove unused files and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 // 튜토리얼: https://reactrouter.com/en/main/start/tutorial
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import Router from './app/router.jsx';
-import './index.css';
+import Router from "./app/router.jsx";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={Router} />
   </React.StrictMode>
--- src/routes/destroy.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "react-router-dom";
-import { deleteUser } from "../connect/connect-api";
-
-export async function action({ params }) {
-    await deleteUser({"id" : params.contactId});
-    return redirect("/");
-}
