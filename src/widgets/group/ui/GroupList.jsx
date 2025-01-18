45c4b6585c3a167a716cee7ebf02e814f580b4d1
전준영 <reus7042@gmail.com>
2025-01-18 12:13:37 +0900

    :sparkles: Add group register popup

--- src/widgets/group/ui/GroupList.jsx
+++ src/widgets/group/ui/GroupList.jsx
@@ -1,15 +1,32 @@
-import { GroupItem } from "@entities/group";
+import { useState } from "react";
 
+import { GroupItem } from "@entities/group";
+import { GroupAddPopup } from "./GroupAddPopup";
 const CONST_GROUP_LIST = [
   { name: "group1", seq: 1 },
   { name: "group2", seq: 2 },
   { name: "group3", seq: 3 },
 ];
 
-export function GroupList() {
+export function GroupList({ isShowGroupAddBtn = false }) {
+  const [showGroupPopup, setShowGroupPopup] = useState(false);
+
+  const toggleGroupAddPopup = () => {
+    setShowGroupPopup(!showGroupPopup);
+  };
+
   return (
     <div>
-      <h1>내 그룹 리스트</h1>
+      {showGroupPopup && <GroupAddPopup handleClick={toggleGroupAddPopup} />}
+      <div className="flex justify-between">
+        <h1>내 그룹 리스트</h1>
+        <div
+          className="flex gap-2 p-2 btn btn-primary"
+          onClick={toggleGroupAddPopup}
+        >
+          그룹 생성
+        </div>
+      </div>
       <div className="flex gap-2">
         {CONST_GROUP_LIST.map((item) => (
           <GroupItem key={item.seq} data={item} />
